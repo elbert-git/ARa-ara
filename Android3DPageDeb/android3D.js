9c43f8050e62066b0f7ae4a68435515ab544961b
Elbert Nathanael <elbertwithane@gmail.com>
2022-01-21 09:17:02 +0800

    check ar func

--- Android3DPageDeb/android3D.js
+++ Android3DPageDeb/android3D.js
@@ -1,2 +1,39 @@
-console.log("aa");
-document.querySelector('a-scene').enterAR()
+const reticle = document.querySelector('[ar-hit-test]');
+
+// function called on a-frame load
+document.querySelector('a-scene').addEventListener('loaded', function () {
+  //check ar functionalities
+  if ("xr" in window.navigator) {
+    /* WebXR can be used! */
+  } else {
+    /* WebXR isn't available */
+    document.getElementsByClassName("a-enter-ar-button")[0].remove();
+  }
+
+  navigator.xr.requestSession("immersive-ar")
+  .then((xrSession) => {
+    xrSession.addEventListener('end', onXRSessionEnded);
+    // Do necessary session setup here.
+    // Begin the session’s animation loop.
+    xrSession.requestAnimationFrame(onXRAnimationFrame);
+  }).catch(function(error) {
+    // "immersive-ar" sessions are not supported
+    console.warn("'immersive-vr' isn't supported, or an error occurred activating VR!");
+    document.getElementsByClassName("a-enter-ar-button")[0].remove();
+  });
+
+
+  // add event listener to ar button click 
+  let a = document.getElementsByClassName("a-enter-ar-button")[0];
+  a.addEventListener("click", function(){ // run this on button click
+    console.log("ar button clicked");
+    document.getElementById("overlay").classList.remove("hide");
+    document.getElementsByTagName("a-scene")[0].setAttribute("background", "color: #ff000000; transparent: true");
+  });
+})
+
+//dismiss android scan env prompt
+function dismissPrompt() {
+  document.getElementById('dismiss-on-tap').remove();
+  console.log('a');
+}
