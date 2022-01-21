6468483168f6579b638c07598aaaa6ca75e569cc
Elbert Nathanael <elbertwithane@gmail.com>
2022-01-21 10:02:54 +0800

    check ar func

--- Android3DPageDeb/android3D.js
+++ Android3DPageDeb/android3D.js
@@ -1,26 +1,20 @@
+//---------------- A FRAME STUFF
 const reticle = document.querySelector('[ar-hit-test]');
 
 // function called on a-frame load
 document.querySelector('a-scene').addEventListener('loaded', function () {
-  //check ar functionalities
-  if ("xr" in window.navigator) {
-    /* WebXR can be used! */
-  } else {
-    /* WebXR isn't available */
-    document.getElementsByClassName("a-enter-ar-button")[0].remove();
-  }
+  //check ar android funcs
 
-  navigator.xr.requestSession("immersive-ar")
-  .then((xrSession) => {
-    xrSession.addEventListener('end', onXRSessionEnded);
-    // Do necessary session setup here.
-    // Begin the session’s animation loop.
-    xrSession.requestAnimationFrame(onXRAnimationFrame);
-  }).catch(function(error) {
-    // "immersive-ar" sessions are not supported
-    console.warn("'immersive-vr' isn't supported, or an error occurred activating VR!");
-    document.getElementsByClassName("a-enter-ar-button")[0].remove();
-  });
+  if (navigator.xr) {
+    navigator.xr.isSessionSupported('immersive-ar')
+    .then((isSupported) => {
+      if (!isSupported) {
+        alert("'immersive-va' isn't supported, or an error occurred activating AR!");
+        document.getElementsByClassName("a-enter-ar-button")[0].remove();
+        document.getElementsByClassName("bottom-ar-prompt")[0].remove();
+      }
+    });
+  }
 
 
   // add event listener to ar button click 
@@ -36,4 +30,45 @@ document.querySelector('a-scene').addEventListener('loaded', function () {
 function dismissPrompt() {
   document.getElementById('dismiss-on-tap').remove();
   console.log('a');
+}
+
+
+
+
+// ----------- apple ar
+function spawnAppleARButton(){
+  let arButton = document.getElementById("apple-ar-button-parent");
+
+  arButton.classList.remove("hide");
+  arButton.classList.add("apple-ar-button-parent");
+  arButton.classList.add("layer-over");
+  
+}
+
+if(getMobileOperatingSystem() == "IOS"){
+  spawnAppleARButton(); 
+}
+
+
+
+
+// --------------- utilit functions
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+  }
+
+  if (/android/i.test(userAgent)) {
+      return "Android";
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+  }
+
+  return "unknown";
 }
